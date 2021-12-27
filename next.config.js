52eb9ddae5509ee646b592976b7e235a93614e72
Giancarlos Isasi <giancarlos@crehana.com>
2021-12-27 15:28:42 -0500

    chore: setup project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const config = {
+  env: {
+    REST_ENDPOINT: process.env.REST_ENDPOINT,
+    GRAPHQL_ENDPOINT: process.env.REST_ENDPOINT,
+  },
+  webpack: config => {
+    config.resolve.plugins = [
+      ...config.resolve.plugins,
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.paths.json',
+      }),
+    ];
+    config.plugins = [...config.plugins];
+
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      include: [/src/],
+      loader: 'graphql-tag/loader',
+    });
+
+    return config;
+  },
+};
+
+module.exports = config;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+
+import '../../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
+export default MyApp;
+
